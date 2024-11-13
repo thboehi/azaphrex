7bb0d045ffb499d889f8d1f84878e9514437d3cc
thboehi <thomaboehi@gmail.com>
2024-11-13 15:39:36 +0100

    Added FX on arrow up and arrow left

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let bg = document.getElementById("background")
 let text = document.getElementById("title")
 let effect = document.getElementById("effect")
 let sub = document.getElementById("hero-sub")
+let stab_head = document.getElementById("banner-ytb-single")
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY
@@ -14,6 +15,80 @@ window.addEventListener('scroll', () => {
 })
 
 
+let animateStab = (element) => {
+    element.animate([
+        { opacity: 1, transform: 'scale(1) rotate(0deg)', filter: 'brightness(1)' },
+        { transform: 'scale(1.3) rotate(30deg)', filter: 'brightness(1)', offset: 0.1 },
+        { transform: 'scale(1.6) rotate(28deg)', filter: 'brightness(1.2)', offset: 0.15 },
+        { transform: 'scale(1.8) rotate(32deg)', filter: 'brightness(1.4)', offset: 0.2 },
+        { transform: 'scale(1.8) rotate(26deg)', filter: 'brightness(0.7)', offset: 0.25 },
+        { transform: 'scale(1.6) rotate(24deg)', filter: 'brightness(1.2)', offset: 0.3 },
+        { transform: 'scale(1.6) rotate(12deg)', filter: 'brightness(1.2)', offset: 0.35 },
+        { transform: 'scale(1.6) rotate(26deg)', filter: 'brightness(1)', offset: 0.4 },
+        { opacity: 1, offset: 0.99 },
+        { opacity: 0, transform: 'scale(1) rotate(0deg)', filter: 'brightness(1)', offset: 1 }
+    ], {
+        duration: 900,
+        fill: 'forwards'
+    });
+};
+
+let animateDistortion = (element) => {
+    element.animate([
+        { opacity: 1, transform: 'scale(1) rotate(0deg)', filter: 'brightness(1)' },
+        { transform: 'scale(1.2) rotate(-3deg)', filter: 'brightness(1.1)', offset: 0.025 },
+        { transform: 'scale(1.5) rotate(-6deg)', filter: 'brightness(1.2)', offset: 0.05 },
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.3)', offset: 0.075 },
+        
+        { transform: 'scale(1.8) rotate(-7deg)', filter: 'brightness(1.4)', offset: 0.1 },
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.5)', offset: 0.125 },
+        { transform: 'scale(1.8) rotate(-7deg)', filter: 'brightness(1.6)', offset: 0.15 },
+        { transform: 'scale(1.8) rotate(-3deg)', filter: 'brightness(1.2)', offset: 0.175 },
+        
+        { transform: 'scale(1.8) rotate(-8deg)', filter: 'brightness(1.4)', offset: 0.2 },
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.3)', offset: 0.225 },
+        { transform: 'scale(1.8) rotate(-5deg)', filter: 'brightness(1.5)', offset: 0.25 },
+        { transform: 'scale(1.8) rotate(-8deg)', filter: 'brightness(1.6)', offset: 0.275 },
+        
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.4)', offset: 0.3 },
+        { transform: 'scale(1.8) rotate(-6deg)', filter: 'brightness(1.2)', offset: 0.325 },
+        { transform: 'scale(1.8) rotate(-9deg)', filter: 'brightness(1.5)', offset: 0.35 },
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.6)', offset: 0.375 },
+        
+        { transform: 'scale(1.8) rotate(-8deg)', filter: 'brightness(1.2)', offset: 0.4 },
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.4)', offset: 0.425 },
+        { transform: 'scale(1.8) rotate(-7deg)', filter: 'brightness(1.6)', offset: 0.45 },
+        
+        { transform: 'scale(1.8) rotate(-10deg)', filter: 'brightness(1.3)', offset: 0.475 },
+        
+        { transform: 'scale(1.8) rotate(-20deg)', filter: 'brightness(1.5)', offset: 0.5 },
+        { transform: 'scale(1.8) rotate(-23deg)', filter: 'brightness(1.2)', offset: 0.525 },
+        { transform: 'scale(1.8) rotate(-27deg)', filter: 'brightness(1.6)', offset: 0.55 },
+        { transform: 'scale(1.8) rotate(-24deg)', filter: 'brightness(1.3)', offset: 0.575 },
+        
+        { transform: 'scale(1.8) rotate(-28deg)', filter: 'brightness(1.2)', offset: 0.6 },
+        { transform: 'scale(1.8) rotate(-25deg)', filter: 'brightness(1.4)', offset: 0.625 },
+        { transform: 'scale(1.8) rotate(-30deg)', filter: 'brightness(1.1)', offset: 0.65 },
+        { transform: 'scale(1.8) rotate(-27deg)', filter: 'brightness(1.3)', offset: 0.675 },
+        
+        { transform: 'scale(1.8) rotate(-23deg)', filter: 'brightness(1.5)', offset: 0.7 },
+        { transform: 'scale(1.8) rotate(-26deg)', filter: 'brightness(1.2)', offset: 0.725 },
+        { transform: 'scale(1.8) rotate(-29deg)', filter: 'brightness(1.4)', offset: 0.75 },
+        { transform: 'scale(1.8) rotate(-25deg)', filter: 'brightness(1.2)', offset: 0.775 },
+        
+        { transform: 'scale(1.8) rotate(-28deg)', filter: 'brightness(1.1)', offset: 0.8 },
+        { transform: 'scale(1.8) rotate(-30deg)', filter: 'brightness(1.3)', offset: 0.825 },
+        { transform: 'scale(1.8) rotate(-23deg)', filter: 'brightness(1)', offset: 0.85 },
+        { transform: 'scale(1.8) rotate(-27deg)', filter: 'brightness(1.5)', offset: 0.875 },
+        
+        { opacity: 1, transform: 'scale(1.8) rotate(-29deg)', filter: 'brightness(1.3)', offset: 0.9 },
+        { opacity: 1, transform: 'scale(1) rotate(0deg)', filter: 'brightness(1)', offset: 0.95 },
+        { opacity: 0, transform: 'scale(1) rotate(0deg)', filter: 'brightness(1)', offset: 1 }
+    ], {
+        duration: 2700,
+        fill: 'forwards'
+    });
+};
 
 window.onkeydown = function(e) {
     let key = e.keyCode
@@ -25,10 +100,12 @@ window.onkeydown = function(e) {
         case 37:
             e.preventDefault()
             stab.play()
+            animateStab(stab_head)
             break
         case 38:
             e.preventDefault()
             distortion.play()
+            animateDistortion(stab_head)
             break
         case 40:
             e.preventDefault()
